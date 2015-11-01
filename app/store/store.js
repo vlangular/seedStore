a803d0b6a60a2ffa2bd6b21eb1fa3523b1db5a1d
vladwho <vladpresedinte@gmail.com>
2015-11-02 00:39:40 +0200

    Add shared service & product controller

--- app/store/store.js
+++ app/store/store.js
@@ -8,19 +8,33 @@ angular.module('seedApp.store', ['ngRoute'])
   .when('/store', {
     templateUrl: 'store/store.html',
     controller: 'StoreCtrl'
+  })
+  .when('/store/:productId', {
+    templateUrl: 'store/product.html',
+    controller: 'ProductCtrl'
   });
-
 
 }])
 
-.controller('StoreCtrl', ['$scope', '$http', function($scope,$http) {
-    $http({
-      method: 'GET',
-      url: '/app/data/seedDB.json'
-    }).then(function successCallback(response) {
-        $scope.seeds = response.data;
-      }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-      });
+.factory('getProductsSrv', function($http) {
+    return {
+        getProducts: function() {
+            var url = '/app/data/seedDB.json';
+            return $http.get(url);
+        }
+    };
+})
+
+.controller('StoreCtrl', ['$scope', 'getProductsSrv', function($scope, getProductsSrv) {
+    getProductsSrv.getProducts().success(function(seeds) {$scope.seeds = seeds});
+}])
+
+.controller('ProductCtrl', ['$scope', '$routeParams', '$filter', 'getProductsSrv', function($scope, $routeParams, $filter, getProductsSrv) {
+    var productId = $routeParams.productId;
+    getProductsSrv.getProducts().success(function(seeds) {
+        var seedsData = seeds;
+        $scope.seed = $filter('filter')(seedsData, function(seeds){
+            return seeds.id == productId;
+        })[0];
+    });
 }]);
