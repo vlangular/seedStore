cb50408c8fb9e819d1f14be54f995e910e339d14
vladwho <vladpresedinte@gmail.com>
2015-11-01 21:16:35 +0200

    Add store

--- app/store/store.js
+++ app/store/store.js
@@ -3,12 +3,24 @@
 angular.module('seedApp.store', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/store', {
+  $routeProvider
+
+  .when('/store', {
     templateUrl: 'store/store.html',
     controller: 'StoreCtrl'
   });
+
+
 }])
 
-.controller('StoreCtrl', [function() {
-
+.controller('StoreCtrl', ['$scope', '$http', function($scope,$http) {
+    $http({
+      method: 'GET',
+      url: '/app/data/seedDB.json'
+    }).then(function successCallback(response) {
+        $scope.seeds = response.data;
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
 }]);
